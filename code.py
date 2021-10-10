d8b7231d9fa7578895678236aeb30e6295a436bb
Saloni Sharma <68701930+saloni1966@users.noreply.github.com>
2021-10-10 12:42:29 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,7 @@
 import plotly.figure_factory as ff
-import csv
 import pandas as pd
+import csv
 
 df = pd.read_csv("data.csv")
-
-fig = ff.create_distplot([df["Weight(Pounds)"].tolist()], ["Weight"],show_hist=False)
+fig = ff.create_distplot([df["Avg Rating"].tolist()], ["Avg Rating"])
 fig.show()
